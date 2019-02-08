67e7ef3947bd0d1ec2d8e6137214dbd1cb999faf
Colin <colin.reitman87@gmail.com>
2019-02-08 10:46:16 -0500

    ADT HW

---
+++ W5D5/DIYADT.rb
@@ -0,0 +1,45 @@
+class Stack
+    def initialize
+        
+    end
+
+    def push(el)
+      
+    end
+
+    def pop
+      
+    end
+
+    def peek
+      
+    end
+  end
+
+  class Queue
+
+    def initialize
+        array = []
+    end
+
+    def enqueue(el)
+
+    end
+
+    def dequeue
+
+    end
+
+    def peek
+
+    end
+
+
+    def map
+
+        
+
+
+    end
+
+  end
